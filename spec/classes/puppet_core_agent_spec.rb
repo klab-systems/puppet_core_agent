9f4c496a93ba669d2204244fade9d9a20ebf93c3
Kevin <114269618+klab-systems@users.noreply.github.com>
2025-03-14 14:49:27 -0400

    Update puppet_core_agent_spec.rb

--- spec/classes/puppet_core_agent_spec.rb
+++ spec/classes/puppet_core_agent_spec.rb
@@ -6,7 +6,7 @@ describe 'puppet_core_agent' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) { { 'forge_api_key' => 'MYFORGEAPIKEY', 'manage_package' => true } }
+      let(:params) { { 'forge_api_key' => 'MYFORGEAPIKEY', 'manage_package' => true, 'version' => '8.11.0' } }
 
       it { is_expected.to compile.with_all_deps }
     end
